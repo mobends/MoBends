152c982239f4e339bc9675da08385ae1a30bcab4
Iwo Plaza <gobbobpl@gmail.com>
2017-12-04 18:38:03 +0100

    Added extended model parts that enable fore-arms.

---
+++ 1.12/src/main/java/net/gobbob/mobends/client/model/ModelPartChildExtended.java
@@ -0,0 +1,71 @@
+package net.gobbob.mobends.client.model;
+
+import net.gobbob.mobends.util.SmoothVector3f;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
+
+public class ModelPartChildExtended extends ModelPartChild{
+	protected ModelPart extension;
+	
+	public ModelPartChildExtended(ModelBase model, boolean register, int texOffsetX, int texOffsetY) {
+		super(model, register, texOffsetX, texOffsetY);
+	}
+	
+	public ModelPartChildExtended(ModelBase model, boolean register)
+	{
+		this(model, register, 0, 0);
+	}
+	
+	public ModelPartChildExtended(ModelBase model, int texOffsetX, int texOffsetY)
+    {
+		this(model, true, texOffsetX, texOffsetY);
+    }
+	
+	public ModelPartChildExtended setExtension(ModelPart modelRenderer) {
+		extension = modelRenderer;
+		return this;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void render(float scale)
+    {
+        if (this.isHidden || !this.showModel) return;
+        if (!this.compiled)
+            this.compileDisplayList(scale);
+        
+        GlStateManager.pushMatrix();
+	        super.postRender(scale);
+	        
+	        GlStateManager.callList(this.displayList);
+	        
+	        if (this.childModels != null)
+	        {
+	            for (int k = 0; k < this.childModels.size(); ++k)
+	            {
+	                ((ModelRenderer)this.childModels.get(k)).render(scale);
+	            }
+	        }
+	        
+	        if(extension != null)
+	        	extension.render(scale);
+        GlStateManager.popMatrix();
+    }
+	
+	@Override
+    @SideOnly(Side.CLIENT)
+    public void postRender(float scale)
+    {
+		super.postRender(scale);
+		
+		if(extension != null)
+        	extension.postRender(scale);
+    }
+}
---
+++ 1.12/src/main/java/net/gobbob/mobends/client/model/ModelPartExtended.java
@@ -0,0 +1,69 @@
+package net.gobbob.mobends.client.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ModelPartExtended extends ModelPart {
+	protected ModelPart extension;
+	
+	public ModelPartExtended(ModelBase model, boolean register, int texOffsetX, int texOffsetY)
+	{
+		super(model, register, texOffsetX, texOffsetY);
+	}
+
+	public ModelPartExtended(ModelBase model, boolean register)
+	{
+		super(model, register);
+	}
+
+	public ModelPartExtended(ModelBase model, int texOffsetX, int texOffsetY)
+	{
+		super(model, texOffsetX, texOffsetY);
+	}
+
+	public ModelPartExtended setExtension(ModelPart modelRenderer) {
+		extension = modelRenderer;
+		return this;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+    public void render(float scale)
+    {
+        if (this.isHidden || !this.showModel) return;
+        if (!this.compiled)
+            this.compileDisplayList(scale);
+        
+        GlStateManager.pushMatrix();
+	        super.postRender(scale);
+	        
+	        GlStateManager.callList(this.displayList);
+	        
+	        if (this.childModels != null)
+	        {
+	            for (int k = 0; k < this.childModels.size(); ++k)
+	            {
+	                ((ModelRenderer)this.childModels.get(k)).render(scale);
+	            }
+	        }
+	        
+	        if(extension != null)
+	        	extension.render(scale);
+        GlStateManager.popMatrix();
+    }
+	
+	@Override
+    public void postRender(float scale)
+    {
+        super.postRender(scale);
+        
+        if(extension != null)
+        	extension.postRender(scale);
+    }
+}
