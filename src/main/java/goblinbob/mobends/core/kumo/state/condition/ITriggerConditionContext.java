2f7184e25df7c9c64fd188e345e36f7d2b29fc08
Iwo Plaza <iwoplaza@gmail.com>
2020-05-10 20:08:57 +0200

    Missing files from the previous commit.

--- src/main/java/goblinbob/mobends/core/kumo/state/condition/ITriggerConditionContext.java
+++ src/main/java/goblinbob/mobends/core/kumo/state/condition/ITriggerConditionContext.java
@@ -1,5 +1,25 @@
 package goblinbob.mobends.core.kumo.state.condition;
 
+import goblinbob.mobends.core.data.EntityData;
+import goblinbob.mobends.core.kumo.state.ILayerState;
+import goblinbob.mobends.core.kumo.state.INodeState;
+
 public interface ITriggerConditionContext
 {
+
+    /**
+     * Returns data for the entity that's being animated.
+     */
+    EntityData<?> getEntityData();
+
+    /**
+     * Returns the layer this condition has to be met on.
+     */
+    ILayerState getLayerState();
+
+    /**
+     * Returns the current node.
+     */
+    INodeState getCurrentNode();
+
 }
