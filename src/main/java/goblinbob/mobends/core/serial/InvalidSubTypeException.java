c138b95d6471a7191f5c54575f2394c04685ad68
Iwo Plaza <iwoplaza@gmail.com>
2020-11-13 16:05:56 +0100

    Added an exception if a specified sub-type hasn't been registered.

--- src/main/java/goblinbob/mobends/core/serial/InvalidSubTypeException.java
+++ src/main/java/goblinbob/mobends/core/serial/InvalidSubTypeException.java
@@ -1,5 +1,18 @@
 package goblinbob.mobends.core.serial;
 
-public class InvalidSubTypeException
+public class InvalidSubTypeException extends RuntimeException
 {
+    private final String subType;
+
+    public InvalidSubTypeException(String subType)
+    {
+        super(String.format("SubType couldn't be found in the registry: '%s'.", subType));
+        this.subType = subType;
+    }
+
+    public String getSubType()
+    {
+        return this.subType;
+    }
+
 }
