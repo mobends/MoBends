c9b207d2fea619481c8ae93ca23be2824a15ad80
Iwo Plaza <iwoplaza@gmail.com>
2021-12-12 03:35:08 +0100

    Upped version to "1.2.0"

--- src/main/java/goblinbob/mobends/standard/main/ModStatics.java
+++ src/main/java/goblinbob/mobends/standard/main/ModStatics.java
@@ -4,7 +4,7 @@ public class ModStatics
 {
     public static final String MODID = "mobends";
     public static final String LOADER = "forge";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     public static final String MCVERSION = "1.12.2";
 
     public static final String VERSION_STRING = VERSION + "/" + MCVERSION + "-" + LOADER;
